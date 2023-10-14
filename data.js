bc835d596f6aecef01655367bfbdd1299304dd32
probro91 <bozdener@gmail.com>
2023-10-14 14:33:10 -0400

    Cleaning up details screen

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ export const jobList = [
         Job: "Stocker",
         Pay: "22",
         location: "5 sec ago",
-        description: "Stock shelves"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       },
       {
         company: "Target",
--- screens/Details.js
+++ screens/Details.js
@@ -1,43 +1,56 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Button } from 'react-native';
 
 const Details = ({route}) => {
     const data = route.params.data;
     return (
-        <View style = {styles.jobContainer}>
-            <Text style={styles.baseText}>
-                <Text style={styles.titleText}>
-                    {data.company}{'\n'}
+        <ScrollView>
+            <View style = {styles.jobContainer}>
+                <Text style={styles.baseText}>
+                    <View style = {{flexDirection: "col"}}>
+                        <View style = {{flexDirection: "row"}}>
+                            <Text style={styles.titleText}>
+                                {data.company}
+                            </Text>
+                            <Button title = "Apply" style = {styles.buttonStyle} onPress = {() => {}}/>
+                        </View>
+                            
+                        <Text style = {{fontSize: 30, paddingVertical: 5}}>
+                            <Text style = {{fontWeight: "bold"}}>{"Pay: "}</Text>
+                            <Text>{"$"}{data.Pay}</Text>
+                        </Text>
+                                
+                                
+                        <Text style = {{fontSize: 30, paddingVertical: 5}}>
+                            <Text style = {{fontWeight: "bold"}}>{"Role: "}</Text>
+                            <Text>{data.Job}</Text>
+                        </Text>
+                                
+                                
+                        <Text style = {{fontSize: 30, paddingVertical: 5}}>
+                            <Text style = {{fontWeight: "bold"}}>{"Location: "}</Text>
+                            <Text>{data.location}</Text>
+                        </Text>
+
+                        <Text style = {{fontSize: 30, paddingVertical: 5}}>
+                            <Text style = {{fontWeight: "bold"}}>{"Description: "}{'\n'}</Text>
+                            <Text>{data.description}</Text>
+                        </Text>
+                    </View>
                 </Text>
-                    
-                <Text style = {{fontSize: 20}}>
-                    <Text>{"Pay: $"}</Text>
-                    <Text>{data.Pay}</Text>
-                </Text>
-                        
-                        
-                <Text style = {{fontSize: 20}}>
-                    <Text>{"Role: "}</Text>
-                    <Text>{data.Job}</Text>
-                </Text>
-                        
-                        
-                <Text style = {{fontSize: 20}}>
-                    <Text>{"Location: "}</Text>
-                    <Text>{data.location}</Text>
-                </Text>
-            </Text>
-        </View>
+            </View>
+        </ScrollView>
     )
 };
 
 const styles = StyleSheet.create({
     baseText: {
         fontFamily: 'Damascus',
+        
       },
       titleText: {
-        fontSize: 40,
+        fontSize: 60,
         fontWeight: 'bold',
       },    
     jobContainer: {
@@ -50,17 +63,10 @@ const styles = StyleSheet.create({
         width: 410,
         paddingLeft: 10,
         paddingVertical: 5,
+        
     },
-    pay: {
-        width: "50%",
-        flexDirection: "row",
-    },
-    job: {
-        width: "50%",
-        flexDirection: "row",
-    },
-    payAndJob: {
-        flexDirection: "row",
+    buttonStyle: {
+        float: "right",
     }
 
     });
