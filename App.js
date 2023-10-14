2160184a5b0e5ed972ae7ad42f9404847b11426f
probro91 <bozdener@gmail.com>
2023-10-14 19:07:54 -0400

    Trying to get authentication working

--- App.js
+++ App.js
@@ -2,24 +2,52 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Jobs from "./screens/Jobs";
 import Details from "./screens/Details";
-import React from 'react';
+import Login from "./screens/auth/Login";
+import SignUp from "./screens/auth/SignUp";
+import React, {useState, useEffect} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { User, onAuthStateChanged } from 'firebase/auth';
+import FIREBASE_AUTH from './screens/auth/FirebaseConfig';
+import firebase from 'firebase/app';
 
 const Stack = createNativeStackNavigator();
 
-export default class App extends React.Component {
-  render() {
-      return (
-        <NavigationContainer >
-          <Stack.Navigator initialRouteName='Jobs'>
-            <Stack.Screen name="Jobs" component={Jobs} options={{ headerShown: true }} />
-            <Stack.Screen name="Details" component={Details} options={{ headerShown: true }} />
-          </Stack.Navigator>
-        </NavigationContainer>
-         
-      );
-  }
+const InsideStack = createNativeStackNavigator();
+
+function InsideLayout() {
+  return (
+    <InsideStack.Navigator>
+      <InsideStack.Screen name="Jobs" component={Jobs} />
+      <InsideStack.Screen name="Details" component={Details} />
+    </InsideStack.Navigator>
+  );
+}
+
+
+export default function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      setUser(user);
+    });
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        {user ? 
+          <Stack.Screen name="Inside" component={InsideLayout} options={{ headerShown: true }}/>
+         : 
+          <>
+            <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
+            <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }}/>
+          </>
+        }
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 const styles = StyleSheet.create({
---
+++ screens/auth/FirebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import {getFirestore} from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCX556nf-acpKu1vU0wd6PVmXdqETpA9cg",
+  authDomain: "elderworks-7f9fd.firebaseapp.com",
+  projectId: "elderworks-7f9fd",
+  storageBucket: "elderworks-7f9fd.appspot.com",
+  messagingSenderId: "252182144062",
+  appId: "1:252182144062:web:539ec501587eb402b19d6f"
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_STORE = getFirestore(FIREBASE_APP);
